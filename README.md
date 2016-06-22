701c88949ba0711ffe21be405013791fe0c9036f
Satyajeet Deshpande <sdeshpande@appdynamics.com>
2016-06-21 19:13:33 -0700

    Adding a base structure to the gerrit-stats project. Adding basic index.jsp with bootstrap starter template.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gerrit-stats
 Gives gerrit stats on a user for his change counts and lines of code over a period of time.
+
+Shows graphs of 
+1. Lines of Code Checked in vs Time
+2. No. of Changes vs Time
+
+Input: LDAP or user whose stats are to be shown.
---
+++ src/main/java/com/appdynamics/tools/gerritstats/EntryPoint.java
@@ -0,0 +1,11 @@
+package com.appdynamics.tools.gerritstats;
+
+public class EntryPoint {
+    public static void main(String[] args) {
+        System.out.println(getResult());
+    }
+
+    public static String getResult() {
+        return "Hello World!!!";
+    }
+}
